f358c8c4f106b6caa7ab326b8e3a3cee5ec7bc80
priyanshu1998 <priyanshutirkey1998@gmail.com>
2021-10-19 21:35:53 +0530

    completed lift simulation code

--- Lift.c
+++ Lift.c
@@ -27,16 +27,87 @@ int main(int argc, char **argv){
 
     LiftInfo L = lifts[liftno-1]; //as liftno is 1 indexed
 
-    printf("# %s %d %d %d\n", argv[0], liftno, shmidLifts, shmidFloors);
-    /*
+//    printf("# %s %d %d %d\n", argv[0], liftno, shmidLifts, shmidFloors);
+
     #pragma clang diagnostic push
     #pragma ide diagnostic ignored "EndlessLoop"
     while(1){
-        P(L.stops)
+        /* CASE WHEN LIFT IS GOING UP */
+        if(L.direction == 1) {
+            int F = L.position - 1;// floor in array are 0 indexed.
+            FloorInfo X = floors[F];
+
+            // Releasing lock for those who want to get down =======
+            P(L.stopsem[F]);
+            while (1) {
+                V(L.stopsem[F]);
+                if (L.stops[F] == 0) { break; }
+                P(L.stopsem[F]);
+                sleep(1);
+            }
+
+            // Releasing lock for those who want to get up =========
+            P(X.upArrow);
+            while (1) {
+                V(X.upArrow);
+                V(X.arithmetic);
+                if (X.waitingToGoUp == 0) {
+                    if(L.position == NFLOOR){
+                        L.direction = -1;
+                    }
+
+                    L.position += L.direction;
+                    break;
+                }
+                P(X.arithmetic);
+                P(X.upArrow);
+                sleep(1);
+            }
+            P(X.arithmetic); // X.arithmetic was 1 before 'P(X.upArrow) line'
+            // release X.arithmetic lock to maintain the state.
+        }
+        /******************END OF CASE******************/
+
+
+
+        /* CASE WHEN LIFT IS GOING DOWN */
+        else/*L.direction == -1*/{
+            int F = L.position - 1;// floor in array are 0 indexed.
+            FloorInfo X = floors[F];
+
+            // Releasing lock for those who want to get down =======
+            P(L.stopsem[F]);
+            while (1) {
+                V(L.stopsem[F]);
+                if (L.stops[F] == 0) { break; }
+                P(L.stopsem[F]);
+                sleep(1);
+            }
+            V(L.stopsem[F]);
+
+            // Releasing lock for those who want to get up =========
+            P(X.downArrow);
+            while (1) {
+                V(X.downArrow);
+                V(X.arithmetic);
+                if (X.waitingToGoDown == 0) {
+                    if(L.position == 1){
+                        L.direction = 1;
+                    }
+                    L.position += L.direction;
+                    break;
+                }
+                P(X.arithmetic);
+                P(X.downArrow);
+                sleep(1);
+            }
+            P(X.arithmetic);
+        }
+        /******************END OF CASE******************/
     }
     #pragma clang diagnostic pop
-     */
 
+    onCtrlC:
     release(lifts, floors);
     return 0;
 }
